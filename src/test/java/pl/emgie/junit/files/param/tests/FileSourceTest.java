9ab24089b7f67f407fc8d0f379381ff304d2d7a6
emgie <macgryczka20@gmail.com>
2020-03-14 13:27:54 +0100

    create FileSource annotation tests

--- src/test/java/pl/emgie/junit/files/param/tests/FileSourceTest.java
+++ src/test/java/pl/emgie/junit/files/param/tests/FileSourceTest.java
@@ -2,6 +2,7 @@ package pl.emgie.junit.files.param.tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import pl.emgie.junit.files.param.FileSource;
 
 public class FileSourceTest {
